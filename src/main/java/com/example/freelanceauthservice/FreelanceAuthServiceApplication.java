598a9a43db853adf2d7ce10fca40d102165ce644
Funfayk082 <funfayk082@yandex.ru>
2024-12-21 19:59:55 +0300

    init

---
+++ src/main/java/com/example/freelanceauthservice/FreelanceAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.freelanceauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelanceAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelanceAuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/freelanceauthservice/FreelanceAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.freelanceauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelanceAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
