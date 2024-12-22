bb151df40dde23891d86846940be6d0969453329
Funfayk082 <funfayk082@yandex.ru>
2024-12-22 15:12:41 +0300

    init: Добавил первоначальные библиотеки, добавил конфиг документации

--- src/main/java/com/example/freelanceauthservice/FreelanceAuthServiceApplication.java
+++ src/main/java/com/example/freelanceauthservice/FreelanceAuthServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FreelanceAuthServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FreelanceAuthServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FreelanceAuthServiceApplication.class, args);
+    }
 
 }
--- src/test/java/com/example/freelanceauthservice/FreelanceAuthServiceApplicationTests.java
+++ src/test/java/com/example/freelanceauthservice/FreelanceAuthServiceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FreelanceAuthServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
